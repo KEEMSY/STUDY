15b0afdfd22558ea1d819e5ea2e47c9f89c2b25c
keemsy <phzzz@naver.com>
2022-12-05 20:51:07 +0900

    Docs: Add Adapter Pattern Add some files about Adapter Pattern

---
+++ GoF/Adapter/Animal.java
@@ -0,0 +1,9 @@
+publci abstract class Animal {
+    protected String name;
+
+    public Animal(String name) {
+        this.name = name;
+    }
+
+    public abstract void sound();
+}
---
+++ GoF/Adapter/User.java
@@ -0,0 +1,15 @@
+import java.util.ArrayList;
+
+public class User {
+    public static void main(String[] args) {
+        ArrayList<Animal> animals = new ArrayList<Animal>();
+        animals.add(new Dog("댕이"));
+        animals.add(new Cat("냐옹이"));
+        animals.add(new Cat("집냐옹이"));
+        animal.add(new TigerAdapter("타이온"));
+
+        animals.forEach(animal -> {
+            animal.sound();
+        })
+    }
+}
