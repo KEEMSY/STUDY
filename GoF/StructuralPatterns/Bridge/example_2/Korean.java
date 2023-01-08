df6878040fd961d6bbd7009e417382a64e47690e
keemsy <phzzz@naver.com>
2023-01-08 20:25:27 +0900

    Update: supplementary example codes

---
+++ GoF/StructuralPatterns/Bridge/example_2/Korean.java
@@ -0,0 +1,11 @@
+package example_2;
+
+public class Korean implements Hello{
+
+    @Override
+    public void greeting() { 
+        System.out.println("안녕하세요.");
+    }
+    
+    
+}
---
+++ GoF/StructuralPatterns/Bridge/example_3/Hello.java
@@ -0,0 +1,7 @@
+package example_3;
+
+// 공통 인터페이스를 만든다. 인터페이스를 통해 계층화된 클래스를 분리한다.
+
+public interface Hello {
+    public void greeting();
+}
